437c87e28ab15983b9f79002bdb1431f7607db76
Ryan Clark <ryan.clark.j@gmail.com>
2015-07-27 17:36:09 -0700

    first

---
+++ test/manual.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+var s3url = process.argv[2];
+
+var keys = require('../lib/keys')(s3url, { objectMode: true });
+var destroy = require('../lib/delete')();
+var starttime = Date.now();
+
+setInterval(function() {
+  var duration = (destroy.deleted / ((Date.now() - starttime) / 1000)).toFixed(2);
+  process.stdout.write('\r\033[K' + destroy.deleted + ' @ ' + duration + '/s');
+}, 500).unref();
+
+keys.pipe(destroy);
