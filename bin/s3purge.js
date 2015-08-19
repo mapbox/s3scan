0f3940bbcd557e56d401212fed4995fb9b9d912f
Ryan Clark <ryan.clark.j@gmail.com>
2015-08-19 06:26:52 -0700

    quiet flag

--- bin/s3purge.js
+++ bin/s3purge.js
@@ -15,15 +15,20 @@ if (!s3url) {
   process.exit(1);
 }
 
+var quiet = process.argv[3] === '--quiet' || process.argv[3] === '-q';
+var interval;
+
 var purge = s3scan.Purge(s3url, agent, function(err) {
-  clearInterval(i);
+  if (!quiet) clearInterval(interval);
   setTimeout(function() {
-    console.log('');
+    if (!quiet) console.log('');
     if (err) throw err;
     else process.exit(0);
   }, 500);
 });
 
-var i = setInterval(function() {
-  process.stdout.write(util.format('\r\033[KDeleted %s @ %s/s', purge.deleted, purge.rate()));
-}, 500);
+if (!quiet) {
+  interval = setInterval(function() {
+    process.stdout.write(util.format('\r\033[KDeleted %s @ %s/s', purge.deleted, purge.rate()));
+  }, 500);
+}
